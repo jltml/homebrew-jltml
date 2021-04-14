ff9814152e164b5a53a17184c2ac8ca8faba8201
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2021-04-14 05:45:05 +0000

    vaccine-finder: add 0.1.6 bottle.

--- vaccine-finder.rb
+++ vaccine-finder.rb
@@ -5,6 +5,12 @@ class VaccineFinder < Formula
   sha256 "67db82650cdd7ca121fdcf01ea1b667a1766417bbb387332d9036b2092687061"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/jltml/homebrew-tap/releases/download/vaccine-finder-0.1.6"
+    sha256 cellar: :any_skip_relocation, catalina:     "2053f3f3f79f0db2753e926c623bc2a680ecd769e82b027822386e5e4ad4685b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3eeb4fe8b1ebb7fdd03b5b0efb821fac078ca487f1ce1f2101619b7b90865d73"
+  end
+
   uses_from_macos "curl"
   uses_from_macos "ruby"
 
