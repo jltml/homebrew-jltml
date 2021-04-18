06a955239e2ce5bb436abbda7cba9cc8b97711db
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2021-04-18 05:22:03 +0000

    vaccine-spotter: update 0.2.4 bottle.

--- vaccine-spotter.rb
+++ vaccine-spotter.rb
@@ -6,9 +6,9 @@ class VaccineSpotter < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/jltml/homebrew-tap/releases/download/vaccine-spotter-0.2.2"
-    sha256 cellar: :any_skip_relocation, catalina:     "cc9dbab2aef421250d11100b1adedff66f5ecaf6d927ab93138dcc6d9ba9a9db"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c583e0518c61bf731d008ec4be7e97062abebfb95d67ae68f635d4b6c86134b5"
+    root_url "https://github.com/jltml/homebrew-tap/releases/download/vaccine-spotter-0.2.4"
+    sha256 cellar: :any_skip_relocation, catalina:     "0f2557ab3db1678bff35eaffcf1cdfee4c67ae38d9b45cbddaa74e4ff5f86965"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8104fac397ecaa95dfd88c1ce78882c7c5367d9382f6565553a8a6486e822be9"
   end
 
   uses_from_macos "curl"
