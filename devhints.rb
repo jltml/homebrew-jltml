51904486be90be39b206af68679dae0aeeb2f80a
Jack MapelLentz <8261330+jltml@users.noreply.github.com>
2021-05-11 22:16:50 +0000

    devhints: update 0.1.2 bottle.

--- devhints.rb
+++ devhints.rb
@@ -6,9 +6,9 @@ class Devhints < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/jltml/homebrew-tap/releases/download/devhints-0.1.1"
-    sha256 cellar: :any_skip_relocation, catalina:     "18df46b65eef95dcf814466fb3e06d072c4635fb9b47d7934d5bdb89e945de1f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7e3e71fd92fa59fd8c16eb265ae12ad1050cea94d838f0c7720094e0eb713c48"
+    root_url "https://github.com/jltml/homebrew-tap/releases/download/devhints-0.1.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "0392a3c5b426ad7a1d16d21e1eb757a4a75d7982c50c6f1f305ae85c3e3ad03c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b4e294b2128f26fba7a17a0a0564db81f34e9fb5b3051bae2671c69e0454fada"
   end
 
   uses_from_macos "ruby"
