8b69676398ac2da29fa9b610f90557e5df65917f
jltml <8261330+jltml@users.noreply.github.com>
2021-07-05 13:58:21 -0500

    Add gem pristine step in lyricsender.rb; roll version back to 0.1.0
    (version rollback so bottles can be regenerated)

--- lyricsender.rb
+++ lyricsender.rb
@@ -1,24 +1,20 @@
 class Lyricsender < Formula
   desc "Send lyrics, line-by-line, using iMessage"
   homepage "https://github.com/jltml/lyricsender"
-  url "https://github.com/jltml/lyricsender/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "b3a4880737d727ed17101fcbb5680456bb5be3c985a30e864043319998fd4de3"
+  url "https://github.com/jltml/lyricsender/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "aa8230ee4884b91cbb9eeac74456e790dbc9661109fb1413cea56714418a98ff"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/jltml/homebrew-tap/releases/download/lyricsender-0.1.1"
-    sha256                               catalina:     "6810808815e27868908740351070993167c7a6188974990acbcbf5f88960b62e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "978ce9b2f0b9eafe1b9b5155bcfcd3827983ff5ddfa42996bf5c3f55e155abf5"
-  end
-
-  uses_from_macos "libxml2"
-  uses_from_macos "libxslt"
+  uses_from_macos "libxml2" => :build
+  uses_from_macos "libxslt" => :build
   uses_from_macos "ruby"
 
   def install
     ENV["GEM_HOME"] = libexec
+    ENV["GEM_PATH"] = libexec
     system "gem", "build", "#{name}.gemspec"
     system "gem", "install", "-V", "#{name}-#{version}.gem", "--no-document", "--platform=ruby", "--", "--use-system-libraries"
+    system "gem", "pristine", "-V", "--all"
     bin.install libexec/"bin/#{name}"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
