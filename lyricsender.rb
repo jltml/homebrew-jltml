ea307bbfb9fea904a50dc6c7bf48c873c041c530
jltml <8261330+jltml@users.noreply.github.com>
2021-07-04 22:34:19 -0500

    Update lyricsender.rb install method & test

--- lyricsender.rb
+++ lyricsender.rb
@@ -10,12 +10,12 @@ class Lyricsender < Formula
   def install
     ENV["GEM_HOME"] = libexec
     system "gem", "build", "#{name}.gemspec"
-    system "gem", "install", "#{name}-#{version}.gem"
+    system "gem", "install", "-V", "#{name}-#{version}.gem", "--no-document", "--platform=ruby", "--", "--use-system-libraries"
     bin.install libexec/"bin/#{name}"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
 
   test do
-    system "true"
+    assert_equal "lyricsender v#{version}", shell_output("#{bin}/lyricsender --version").strip
   end
 end
