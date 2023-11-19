8b9f4ad1680f6fd13955820851de94cfce4b5b24
jltml <8261330+jltml@users.noreply.github.com>
2023-11-18 23:27:17 -0500

    Package artshop as gem (artshop v0.0.5)

--- artshop.rb
+++ artshop.rb
@@ -1,15 +1,20 @@
 class Artshop < Formula
   desc "Command-line interface for Max's art store at mapellentz.art"
   homepage "https://github.com/jltml/artshop"
-  url "https://github.com/jltml/artshop/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "f2ccaae08173451c1db85755fd3a500136bf96e17d867856c9a14c5c4d667e18"
-  license "MIT"
+  url "https://github.com/jltml/artshop/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "1800a894ad259ac47e44396a4c7c6106ae61352025e09a36e909ce7de03ffb49"
 
   depends_on "ruby"
 
   def install
-    bin.install "bin/artshop"
-    lib.install Dir["lib/*"]
+    ENV["GEM_HOME"] = libexec
+    ENV["GEM_PATH"] = libexec
+    system "gem", "build", "#{name}.gemspec"
+    system "gem", "install", "-V", "#{name}-#{version}.gem", "--no-document"
+    (libexec/"lib").install Dir["lib/*"]
+    bin.install "exe/#{name}"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    chmod 0555, libexec/"bin"/name
   end
 
   def caveats
